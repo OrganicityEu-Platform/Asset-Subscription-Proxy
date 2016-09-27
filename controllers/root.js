659062560e31e667f7628f544e0156fe6e2f4bf5
Dennis Boldt <boldt@itm.uni-luebeck.de>
2016-09-27 04:27:36 -0400

    Clean up code + log warnings

--- controllers/root.js
+++ controllers/root.js
@@ -18,10 +18,10 @@ var Root = (function () {
 	var payload = 'grant_type=client_credentials&client_id=' + config.client_id + '&client_secret=' + config.client_secret;
 
 	proxy.sendData('https', options, payload, res, function (status, responseText) {
-                console.log('Access Token received');
+//                console.log('Access Token received');
    		var token = JSON.parse(responseText);
     		req.access_token = token.access_token;
-		console.log(req.access_token);
+//		console.log(req.access_token);
 		next();
 	});
 
@@ -38,8 +38,6 @@ var Root = (function () {
 
 	options.headers['authorization'] = 'Bearer ' + req.access_token;
 
-	console.log('Options:', options);
-
         if (req.body.length > 0) {
             //log.info(req.body.toString());
             var assets;
@@ -48,7 +46,7 @@ var Root = (function () {
                 assets = JSON.parse(req.body);
             } catch (e) {
                 log.error(e);
-                res.send(400, e);
+		res.status(400).send(e);
             }
             if (assets != undefined && assets.data != undefined && assets.data.length >= 1) {
                 for (var i = 0; i < assets.data.length; i++) {
@@ -59,7 +57,7 @@ var Root = (function () {
                 }
             }
         } else {
-            res.send(400, "No Asset Info");
+	    res.status(400).send("No Asset Info");
         }
         return;
     }
@@ -68,26 +66,26 @@ var Root = (function () {
         options.path = req.url + 'v2/entities/' + assetId + '/attrs';
         // options.path = req.url + 'v2/entities/' + assetId + '?type=' + type; //not implemented yet at Orion
         options.method = 'POST';
-        log.info("Asset Creating:" + assetId);
+        log.info("Asset Creating: " + assetId);
         delete asset.id;
         delete asset.type;
         var payload = JSON.stringify(asset);
         proxy.sendData(config.asset_directory_protocol, options, payload, res,
             function (status, e) {
                 if (status == 201 || status == 204) {
-                    log.info("Asset Updated:" + this.assetId);
-                    res.send(200, "Asset Updated:" + this.assetId);
+                    log.info("Asset Updated: " + this.assetId);
+                    res.status(200).send("Asset Updated:" + this.assetId);
                     return;
                 }
                 log.error(status);
                 log.error(e);
-                res.send(400, e);
+                res.status(400).send(e);
                 return;
             }.bind({assetId: assetId}),
             function (status, e) {
                 if (status == 201 || status == 204) {
                     log.info("Asset Updated:");
-                    res.send(200, "Asset Updated");
+                    res.status(200).send("Asset Updated");
                     return;
                 } else if (status == 404) {
                     this.asset.id = this.assetId;
@@ -97,38 +95,38 @@ var Root = (function () {
                     proxy.sendData(config.asset_directory_protocol, this.options, payload, this.res,
                         function (status, e) {
                             if (status == 201 || status == 204) {
-                                log.info("Asset Created:" + this.assetId);
-                                res.send(200, "Asset Created:" + this.assetId);
+                                log.info("Asset Created: " + this.assetId);
+                                res.status(200).send("Asset Created:" + this.assetId);
                                 return;
                             }
                             log.error(status);
                             log.error(e);
-                            res.send(400, e);
+                            res.status(400).send(e);
                             return;
                         }.bind({assetId: assetId}),
                         function (status, e) {
                             if (status == 201 || status == 204) {
                                 log.info("Asset Created:");
-                                res.send(200, "Asset Created");
+                                res.status(200).send("Asset Created");
                                 return;
                             } else if (status == 404) {
                                 this.asset.id = this.assetId;
                                 this.asset.type = this.type;
-
                             }
                             log.error(status);
                             log.error(e);
                             log.error(this.asset);
-                            res.send(400, e);
+                            res.status(400).send(e);
                             return;
                         }.bind({assetId: assetId, asset: asset, type: type, options: options, res: res, req:req})
                     );
-                }
-                log.error(status);
-                log.error(e);
-                log.error(this.asset);
-                res.send(400, e);
-                return;
+                } else {
+                	log.error(status);
+                	log.error(e);
+                	log.error(this.asset);
+                	res.status(400).send(e);
+                	return;
+		}
             }.bind({assetId: assetId, asset: asset, type: type, options: options, res: res, req:req})
         );
         return
